427a9015ac89e1fd9f1ffe4f6381c47067f631b8
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-01-17 15:36:28 +0800

    fix: Used React fetch instead of axios

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import axios from 'axios';
 
 class AlbumList extends Component {
   componentWillMount() {
-    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response));
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((response) => response.json())
+      .then((responseData) => console.log(responseData));
   }
 
   render() {
