aba5b76b66dff2027cedf89cdebed758b0eebe9d
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-01-17 16:18:53 +0800

    feat: Added AlbumDetail component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
     fetch('https://rallycoding.herokuapp.com/api/music_albums')
       .then((response) => response.json())
-      .then((responseData) => console.log(responseData));
+      .then((responseData) => this.setState({ albums: responseData }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => 
+      <AlbumDetail key={album.title} album={album} />
+    );
   }
 
   render() {
+    console.log(this.state);
+
     return (
       <View>
-        <Text>Album List!!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
